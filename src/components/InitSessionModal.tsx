29bcebc34a4530011db37969b33d44e629840ac5
Anthony Hinchliff <anthony.hinchliff@gmail.com>
2022-07-30 09:47:16 +1000

    add confirm pin input & show validation prompts to user

--- src/components/InitSessionModal.tsx
+++ src/components/InitSessionModal.tsx
@@ -23,6 +23,7 @@ const InitSessionModal: React.FC<InitSessionModalProps> = ({
   setIsOpen,
 }) => {
   const [pin, setPin] = useState<string>("");
+  const [confirmPin, setConfirmPin] = useState<string>("");
   const [initSession, { isError, isLoading }] = useInitSessionMutation();
   const { getSessionNameForId } = useSession();
 
@@ -52,12 +53,16 @@ const InitSessionModal: React.FC<InitSessionModalProps> = ({
 
   const displayName = getSessionNameForId(sessionId);
 
+  const pinContainsNonDigitChars = pin.length > 0 && !!pin.match(/^([^0-9]*)$/);
+
+  const validPin = !!pin.match(/^[0-9]{6}$/) && pin === confirmPin;
+
   return (
     <IonContent>
       <IonModal isOpen={isOpen}>
         <div className="flex flex-col content-center justify-center h-full p-10">
           <IonText color="light">
-            <h1 className="mx-auto text-lg text-center">
+            <h1 className="mx-auto text-xl text-center">
               Initialize {displayName}
             </h1>
           </IonText>
@@ -68,10 +73,13 @@ const InitSessionModal: React.FC<InitSessionModalProps> = ({
               </h1>
             </IonText>
           )}
+          <p className="mt-10 text-sm text-center">
+            Choose a 6 digit pin for your card
+          </p>
           <IonItem className="my-7">
             <IonInput
               value={pin}
-              placeholder="Password"
+              placeholder="Pin"
               type="password"
               disabled={isLoading}
               className="text-white"
@@ -79,7 +87,38 @@ const InitSessionModal: React.FC<InitSessionModalProps> = ({
               onKeyDown={handleOnKeyDown}
             ></IonInput>
           </IonItem>
-          <div className="flex flex-row justify-evenly">
+          {pinContainsNonDigitChars && (
+            <IonText color="danger">
+              <h1 className="mx-auto text-center">
+                Your pin can only contain digits
+              </h1>
+            </IonText>
+          )}
+          {pin.length > 6 && (
+            <IonText color="danger">
+              <h1 className="mx-auto text-center">
+                Your pin must be 6 characters
+              </h1>
+            </IonText>
+          )}
+          <IonItem className="my-7">
+            <IonInput
+              value={confirmPin}
+              placeholder="Confirm Pin"
+              type="password"
+              disabled={isLoading}
+              className="text-white"
+              onIonChange={(e) => setConfirmPin(e?.detail?.value ?? "")}
+              onKeyDown={handleOnKeyDown}
+            ></IonInput>
+          </IonItem>
+
+          {!!confirmPin.length && confirmPin !== pin && (
+            <IonText color="danger">
+              <h1 className="mx-auto text-center mb-10">Pins do not match</h1>
+            </IonText>
+          )}
+          <div className="flex flex-row justify-evenly mt-10">
             <IonButton
               color="medium"
               fill="clear"
@@ -88,7 +127,7 @@ const InitSessionModal: React.FC<InitSessionModalProps> = ({
             >
               Cancel
             </IonButton>
-            <IonButton onClick={handleInit} disabled={isLoading}>
+            <IonButton onClick={handleInit} disabled={isLoading || !validPin}>
               Initialize
             </IonButton>
           </div>
