90bfb487f34cd6b6ad9310d9f69566afe6e9783e
Nate Maile <nate.maile@gridplus.io>
2022-08-11 13:59:44 -0500

    formatter?

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,9 +1,7 @@
 import { isPlatform } from "@ionic/react";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = isPlatform("capacitor")
-  ? "https://jumpbox.gridpl.us/"
-  : "/";
+const baseUrl = isPlatform("capacitor") ? "https://jumpbox.gridpl.us/" : "/";
 const bridgeUrl = "https://jumpbox.gridpl.us:443/phonon/";
 
 export const api = createApi({
