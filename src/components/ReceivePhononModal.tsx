76a91eb0526e573d2978ea80a98fe0633149e0fa
Nathaniel Maile <npmaile@gmail.com>
2022-09-15 01:48:41 -0400

    add copy button to cardID on receive modal

--- src/components/ReceivePhononModal.tsx
+++ src/components/ReceivePhononModal.tsx
@@ -1,13 +1,19 @@
-import { IonButton, IonModal } from "@ionic/react";
-import React from "react";
+import { IonButton, IonModal, IonIcon } from "@ionic/react";
+import { copyOutline } from "ionicons/icons";
+import React, { useState } from "react";
 import QRCode from "qrcode.react";
 import { useSession } from "../hooks/useSession";
 
 export default function ReceivePhononModal({ isModalVisible, hideModal }: any) {
   const { sessionId } = useSession();
-
+  const [copiedText, setCopiedText] = useState("");
   const handleSubmit = () => {
     hideModal();
+  };
+
+  const handleIDClicked = async () => {
+    await navigator.clipboard.writeText(sessionId);
+    setCopiedText("copied!");
   };
 
   const handleOnKeyDown = (event: any): void => {
@@ -31,8 +37,14 @@ export default function ReceivePhononModal({ isModalVisible, hideModal }: any) {
           className="mx-auto"
           includeMargin
         />
-        <p className="text-l font-bold text-center text-gray-400 uppercase">
-          {sessionId}
+        <p
+          className="text-l font-bold text-center text-gray-400 uppercase"
+          onClick={handleIDClicked}
+        >
+          {sessionId}&nbsp;&nbsp;
+          <IonIcon slot="end" icon={copyOutline} />
+          <br />
+          {copiedText}
         </p>
         <IonButton
           key="submit"
