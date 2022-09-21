2badab0be2e936f6abac3c35f73fa78b7dcd93a6
Anthony Montalbano <anthony.montalbano@gridplus.io>
2022-09-21 12:18:26 -0400

    detailed message for phonon pairing bug.

--- src/components/SendPhononModal.tsx
+++ src/components/SendPhononModal.tsx
@@ -30,6 +30,7 @@ const SendPhononModal: React.FC<{
 }) => {
   const { sessionId } = useSession();
   const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessageDetails, setErrorMessageDetails] = useState("");
   const [sendPhonon, { isLoading: isSending }] = useSendPhononMutation();
   const [pair, { isLoading: isPairing }] = usePairMutation();
   const chain = CHAINS[selectedPhonon.ChainID] ?? null;
@@ -71,6 +72,17 @@ const SendPhononModal: React.FC<{
         })
         .catch((err) => {
           setErrorMessage("PAIRING ERROR: " + String(err.data));
+
+          // set error message details
+          if (
+            String(err.data).trim().toLowerCase() ===
+            String("timeout").toLowerCase().trim()
+          ) {
+            setErrorMessageDetails(
+              "There's a known issue that is preventing the pairing of a remote card after a previous Phonon was sent to that same remote card. This will be resolved in a future release."
+            );
+          }
+
           // console.error(err);
         });
     } catch (err) {
@@ -89,8 +101,11 @@ const SendPhononModal: React.FC<{
             chain ? chain.ticker : "ERR"
           }`}
         </p>
-        <p className="font-bold text-xl text-center text-red-400 uppercase mt-2">
-          {errorMessage}
+        <p className="font-bold text-xl text-red-400 mt-2">
+          <div className="uppercase text-center">{errorMessage}</div>
+          {errorMessageDetails && (
+            <span className="text-sm block mt-2">{errorMessageDetails}</span>
+          )}
         </p>
 
         {isPairing ? (
@@ -100,63 +115,61 @@ const SendPhononModal: React.FC<{
             <span className="font-bold ml-1">{getValues("cardId")}</span>...
           </div>
         ) : isSending ? (
-          isConnectedToServer ? (
-            <div className="text-xl mt-8 mx-auto flex items-center">
-              <IonSpinner className="mr-2" />
-              Sending{" "}
-              <span className="font-bold ml-1">
-                {isGreaterThan(selectedPhonon.Denomination, 0) ? (
-                  isNativePhonon(selectedPhonon) ? (
-                    selectedPhonon.Denomination
-                  ) : (
-                    weiToEth(selectedPhonon.Denomination)
-                  )
+          <div className="text-xl mt-8 mx-auto flex items-center">
+            <IonSpinner className="mr-2" />
+            Sending{" "}
+            <span className="font-bold ml-1">
+              {isGreaterThan(selectedPhonon.Denomination, 0) ? (
+                isNativePhonon(selectedPhonon) ? (
+                  selectedPhonon.Denomination
                 ) : (
-                  <IonSpinner />
-                )}{" "}
-                {chain?.ticker}
-              </span>{" "}
-              to
-              <span className="font-bold ml-1">{getValues("cardId")}</span>...
-            </div>
-          ) : (
-            <form
-              className="flex flex-col mt-12"
-              onSubmit={handleSubmit(onSubmit)}
-            >
-              <input
-                className="text-bold p-2 text-xl text-white bg-zinc-800 shadow-inner"
-                placeholder="Recipient Card ID"
+                  weiToEth(selectedPhonon.Denomination)
+                )
+              ) : (
+                <IonSpinner />
+              )}{" "}
+              {chain?.ticker}
+            </span>{" "}
+            to
+            <span className="font-bold ml-1">{getValues("cardId")}</span>...
+          </div>
+        ) : isConnectedToServer ? (
+          <form
+            className="flex flex-col mt-12"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <input
+              className="text-bold p-2 text-xl text-white bg-zinc-800 shadow-inner"
+              placeholder="Recipient Card ID"
+              disabled={isLoading}
+              {...register("cardId", {
+                required: true,
+              })}
+            />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-x-3 mt-4">
+              <IonButton
+                key="submit"
+                size="large"
+                fill="solid"
+                expand="full"
+                color="primary"
+                onClick={handleSubmit(onSubmit)}
                 disabled={isLoading}
-                {...register("cardId", {
-                  required: true,
-                })}
-              />
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-x-3 mt-4">
-                <IonButton
-                  key="submit"
-                  size="large"
-                  fill="solid"
-                  expand="full"
-                  color="primary"
-                  onClick={handleSubmit(onSubmit)}
-                  disabled={isLoading}
-                >
-                  SEND
-                </IonButton>
-                <IonButton
-                  size="large"
-                  expand="full"
-                  fill="clear"
-                  color="medium"
-                  onClick={destroyModal}
-                  disabled={isLoading}
-                >
-                  CANCEL
-                </IonButton>
-              </div>
-            </form>
-          )
+              >
+                SEND
+              </IonButton>
+              <IonButton
+                size="large"
+                expand="full"
+                fill="clear"
+                color="medium"
+                onClick={destroyModal}
+                disabled={isLoading}
+              >
+                CANCEL
+              </IonButton>
+            </div>
+          </form>
         ) : (
           <IonButton
             size="large"
